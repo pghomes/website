61ebeec49b3ddf00017273ed840dd173891c62b5
pghomes <pghomesandapartments@gmail.com>
2022-11-18 15:17:29 +0100

    Heroku config

--- PGHomes_website/settings.py
+++ PGHomes_website/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
+    'whitenoise.runserver_nostatic', 
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'easy_thumbnails'
